d88e9bc0730f1ce802c51c00793f3f63f8381054
Shahid Nazir <shahidnazirmhd@gmail.com>
2024-11-27 05:50:53 +0000

    first commit

---
+++ src/test/java/in/snm/personal_service/PersonalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.snm.personal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
